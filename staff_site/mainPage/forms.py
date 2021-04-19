217f840f83c2f35a0270c9dc9dd583da27ce60c6
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-04-19 21:52:02 +0300

    UPDATE urls.py for the project

--- staff_site/mainPage/forms.py
+++ staff_site/mainPage/forms.py
@@ -1,6 +1,12 @@
 from django_otp.forms import OTPAuthenticationForm
 from django import forms
+from django_otp.models import Device, VerifyNotAllowed
+
 
 class SimpleOTPAuthenticationForm(OTPAuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        super(SimpleOTPAuthenticationForm, self).__init__(args, kwargs)
+
     otp_device = forms.CharField(required=False, widget=forms.HiddenInput)
     otp_challenge = forms.CharField(required=False, widget=forms.HiddenInput)
