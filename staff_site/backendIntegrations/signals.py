953ff599666af883fadd116a29ea94496c6140b4
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-04-23 04:05:54 +0300

    ADDED: new signals for lock model

--- staff_site/backendIntegrations/signals.py
+++ staff_site/backendIntegrations/signals.py
@@ -1,11 +1,15 @@
 import datetime
 import secrets
+from io import BytesIO
 
-from django.db.models.signals import pre_save, post_save
+from django.core.files import File
+
+from manufacturing.pdf_generator import PdfGenerator
+from django.db.models.signals import pre_save, post_save, post_delete
 from django.dispatch import receiver
 from hashlib import sha1
 from random import randint
-from .models import RegisterLock, RegisterCard, RegisterKey
+from .models import RegisterLock, RegisterCard, RegisterKey, LockManuals, LockGeneratedUserFiles
 from django.core import signing
 from django.core.signing import Signer
 MIN_CODE = 100000
@@ -52,24 +56,6 @@ def secure_code(key: RegisterKey, code: int):
     return signing.dumps(data_to_sign)
 
 
-@receiver(post_save, sender=RegisterLock, weak=False)
-def create_linking_code(sender: RegisterKey.__class__, instance, created, **kwargs):
-    """Generates and saves a random number in the linking_code field just before saving lock in database.
-    """
-    if created:
-        new_lock = instance
-        #print(new_lock)
-        id_string = str(new_lock.id)
-        #print(id_string)
-        # Define our random string alphabet (notice I've omitted I,O,etc. as they can be confused for other characters)
-        upper_alpha = "ABCDEFGHJKLMNPQRSTVWXYZ"
-        # Create an 8 char random string from our alphabet
-        random_str = "".join(secrets.choice(upper_alpha) for i in range(8))
-        # Append the ID to the end of the random string
-        new_lock.linking_code = (random_str + id_string)[-8:]
-        new_lock.save()
-
-
 @receiver(pre_save, sender=RegisterKey, weak=False)
 def hash_code(sender: RegisterKey.__class__, **kwargs):
     """Generates and saves a random number in the code field. Hashes generated code of key just before saving it in
@@ -79,8 +65,75 @@ def hash_code(sender: RegisterKey.__class__, **kwargs):
     if not new_key.code:
         code = randint(MIN_CODE, MAX_CODE)
         new_key.code = code
+        new_key.code_secure = secure_code(new_key, code)
+        new_key.hash_code = sha1(str(code).encode('utf-8')).hexdigest()
     else:
         code = new_key.code
         new_key.created_manually = True
-    new_key.code_secure = secure_code(new_key, code)
-    new_key.hash_code = sha1(str(code).encode('utf-8')).hexdigest()
+        new_key.code_secure = secure_code(new_key, code)
+        new_key.hash_code = sha1(str(code).encode('utf-8')).hexdigest()
+
+
+@receiver(post_save, sender=RegisterLock, weak=False)
+def generate_user_file(sender: RegisterLock.__class__, instance, created, **kwargs):
+    """Generates and saves a random number in the linking_code field just before saving lock in database.
+    """
+
+    if created:
+        id_string = str(instance.id)
+        # Define our random string alphabet (notice I've omitted I,O,etc. as they can be confused for other characters)
+        upper_alpha = "ABCDEFGHJKLMNPQRSTVWXYZ"
+        # Create an 8 char random string from our alphabet
+        random_str = "".join(secrets.choice(upper_alpha) for i in range(8))
+        # Append the ID to the end of the random string
+        instance.linking_code = (random_str + id_string)[-8:]
+        instance.save()
+
+        key = RegisterKey(
+            lock=instance,
+            access_start=datetime.datetime.now(),
+            access_stop=datetime.datetime(2099, 12, 31, 23, 59, 59),
+            is_master=True
+        )
+        key.save()
+
+        if str(instance.version) == '1':
+            version = 'Ethernet'
+        elif str(instance.version) == '2':
+            version = 'Wi-Fi'
+        else:
+            version = 'Custom'
+
+        generated_file = PdfGenerator(
+            data={
+                "linking_code": instance.linking_code,
+                "hash_id": instance.hash_id,
+                "version": version,
+                "firmware": instance.firmware,
+                "uuid": instance.uuid,
+                "created_at": instance.created_at,
+                "keys": key.code
+            },
+            pdf_template_path='pdf_generator/owner_file.html').generate()
+        filename = "PDF_USER_GUIDE{}.pdf".format(str(instance.id))
+        db_gen = LockGeneratedUserFiles(
+            filename=filename,
+            lock=instance,
+            file=File(BytesIO(generated_file), name=filename))
+        db_gen.save()
+
+
+@receiver(post_delete, sender=LockManuals)
+def remove_file_from_s3(sender, instance, using, **kwargs):
+    try:
+        instance.file.delete(save=False)
+    except Exception as e:
+        pass
+
+
+@receiver(post_delete, sender=LockGeneratedUserFiles)
+def remove_file_from_s3(sender, instance, using, **kwargs):
+    try:
+        instance.file.delete(save=False)
+    except Exception as e:
+        pass
